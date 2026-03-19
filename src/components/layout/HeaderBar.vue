d42445faebdebc3f5adf3e731d6c33c002941895
Fr0st16 <enzociuffa16@gmail.com>
2026-03-19 11:42:02 +0100

    feats(add: page connexion/inscription et gestion du guard et auth.store)

--- src/components/layout/HeaderBar.vue
+++ src/components/layout/HeaderBar.vue
@@ -26,13 +26,25 @@
         </NButton>
       </NSpace>
       <NSpace align="center" :size="16">
-        <NText depth="3">Renseigner le user connecté ici</NText>
-        <NButton size="small">Déconnexion</NButton>
+        <NText depth="3">{{ user?.username }} est connecté</NText>
+        <NButton size="small" @click="handleLogout">Déconnexion</NButton>
       </NSpace>
     </NSpace>
   </NLayoutHeader>
 </template>
 
 <script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useRouter } from 'vue-router'
+
+import { useAuthStore } from '@/stores/auth.store'
 const apiBaseUrl = import.meta.env.VITE_API_BASE_URL as string
+const router = useRouter()
+const AuthStore = useAuthStore()
+const { user } = storeToRefs(AuthStore)
+
+const handleLogout = () => {
+  AuthStore.logout()
+  router.push('/connexion')
+}
 </script>
--- src/router.ts
+++ src/router.ts
@@ -1,18 +1,51 @@
+import { ref } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomePage from './pages/HomePage.vue'
+import PageConnexion from './pages/PageConnexion.vue'
+import PageInscription from './pages/PageInscription.vue'
+import { useAuthStore } from './stores/auth.store'
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    requiresAuth?: boolean
+  }
+}
+
+export const isAuthenticated = ref(false)
 
 export const ROUTES = {
   HOME: '/',
+  CONNEXION: '/connexion',
+  INSCRIPTION: '/inscription',
 } as const
 
 const routes = [
   { path: ROUTES.HOME, component: HomePage, meta: { requiresAuth: true } },
+  {
+    path: ROUTES.CONNEXION,
+    component: PageConnexion,
+    meta: { requiresAuth: false },
+  },
+  {
+    path: ROUTES.INSCRIPTION,
+    component: PageInscription,
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+router.beforeEach((to) => {
+  const auth = useAuthStore()
+  if (to.meta.requiresAuth && !auth.isAuthenticated) {
+    return ROUTES.CONNEXION
+  } else if (!to.meta.requiresAuth && auth.isAuthenticated) {
+    return ROUTES.HOME
+  } else {
+    return true
+  }
 })
 
 export default router
